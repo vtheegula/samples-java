7f590cdb71e195bf78cf3ac7f61aa38792740795
vtheegula <vishal.theegula@gmail.com>
2021-01-22 16:09:50 -0600

    Added a semicolon at the end, you're welcome.

--- Basic/cordapp-example/contracts/src/main/java/net/corda/samples/example/states/IOUState.java
+++ Basic/cordapp-example/contracts/src/main/java/net/corda/samples/example/states/IOUState.java
@@ -70,4 +70,4 @@ public class IOUState implements LinearState, QueryableState {
     public String toString() {
         return String.format("IOUState(value=%s, lender=%s, borrower=%s, linearId=%s)", value, lender, borrower, linearId);
     }
-}
+};
